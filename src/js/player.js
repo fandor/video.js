4d6025242a8e2e76bc73378e300ef84eb6ef5c14
Jamie Weber <jamie@fandor.com>
2016-08-04 16:47:35 -0700

    Not sure where this method came from.

--- src/js/player.js
+++ src/js/player.js
@@ -1248,10 +1248,6 @@ class Player extends Component {
     return this;
   }
 
-  as3() {
-    this.techCall_('change');
-    return this;
-  }
   /**
    * Check if the player is paused
    * ```js
